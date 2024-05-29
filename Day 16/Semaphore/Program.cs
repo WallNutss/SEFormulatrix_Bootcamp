55f75836a825b8688a4cd4bf07b20f799f56880e
SE-105\Batch 10 <wallnuts73@gmail.com>
2024-05-29 10:56:43 +0700

    adding day16.StreamHandler, example of decoding and reconstructuring text buffer

---
+++ Day 16/Semaphore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Security.AccessControl;
+
+
+class Program{
+    static SemaphoreSlim semaphoreSlim = new(2);
+    static async Task Main(){
+        // Making 10 new task
+        Task[] tasks = new Task[10];
+        for(int i=0;i<tasks.Length;i++){
+            int index = i;
+            tasks[index] = Task.Run(async ()=> await DoWork(index));
+        }
+        await Task.WhenAll(tasks);
+    }
+    static async Task DoWork(int index){
+        Console.WriteLine($"Task {index} started");
+        
+        await semaphoreSlim.WaitAsync();
+
+        await Task.Delay(500);
+        Console.WriteLine($"Task {index} processing");
+        await Task.Delay(500);
+        semaphoreSlim.Release();
+        Console.WriteLine($"Task {index} ended");
+    }
+}
---
+++ Day 16/StreamWriter/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+
+
+class Program{
+    static void Main(){
+        using(StreamWriter streamWriter = new("trety.txt")){
+            
+        }
+    }
+}
