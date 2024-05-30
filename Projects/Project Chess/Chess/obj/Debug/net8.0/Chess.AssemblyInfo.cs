be33a73027b35eb8cefbaa9dd6c6ed74d50779b7
Muhammad Juniarto <wallnuts73@gmail.com>
2024-05-30 15:23:51 +0700

    adding git .ignore to ignore the /obj folder

--- Projects/Project Chess/Chess/obj/Debug/net8.0/Chess.AssemblyInfo.cs
+++ Projects/Project Chess/Chess/obj/Debug/net8.0/Chess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411b30899760769ae18a417caa194efea7148bad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70431cfaa9f25535f2fc83eac5d4a71c49f4577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
