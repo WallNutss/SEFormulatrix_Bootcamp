94ee5cb716cc1da5e2e0b5d5e89033073d857c6a
Muhammad Juniarto <wallnuts73@gmail.com>
2024-06-04 18:31:20 +0700

    medium fix, board printing back to normal, but all still in scrambles

--- Projects/Project Chess/Chess/ConsoleRendererUI/BoardRenderer.cs
+++ Projects/Project Chess/Chess/ConsoleRendererUI/BoardRenderer.cs
@@ -8,10 +8,10 @@ using Chess.PlayerDatas;
 namespace Chess.Render;
 
 public static class BoardRenderer{
-    // private int _widthBoardDimension; 
-    // private int _heightBoardDimension;
-    // private int _widthBoardDimensionRender; 
-    // private int _spaces;
+    private static int _widthBoardDimension = 8; 
+    private static int _heightBoardDimension = 8;
+    private static int _widthBoardDimensionRender = 17; 
+    private static int _spaces = 4;
 
 
     private static string GetPieceSymbol(Piece piece)
@@ -27,12 +27,21 @@ public static class BoardRenderer{
                 _ => "N"
             };
         }
+    private static string GetPieceColor(Piece piece)
+        {
+            return piece.pieceColor switch
+            {
+                ColorType.White   =>     "W",
+                ColorType.Black   =>     "B",
+                _ => "N"
+            };
+        }
 
 
     // internal string firstStringBoard = "      1    2    3    4    5    6    7    8\n";
     // internal string oddStringBoard =   "    + -- + -- + -- + -- + -- + -- + -- + -- +\n";
    
-    // public BoardRenderer(int heightDimension, int widthDimension){
+    // public staticBoardRenderer(int heightDimension, int widthDimension){
     //     _widthBoardDimension = widthDimension;
     //     _heightBoardDimension = heightDimension;
     //     _widthBoardDimensionRender = 41; // 45 Column Console
@@ -40,7 +49,7 @@ public static class BoardRenderer{
     // }
     public static void PrintBoard(List<Piece> playerDatas){
         
-        for(int y=1,dataIndex=1;y<=(8*2)+1; y++){
+        for(int y=1,dataIndex=1;y<=(_heightBoardDimension*2)+1; y++){
             // List<Piece> Take all the pieces
             if(y%2==0){
                 List<Piece> pieceWithData = playerDatas.Where(p => p.pos.y == dataIndex).ToList();
@@ -59,7 +68,7 @@ public static class BoardRenderer{
 
     private static void PrintEvenStringBoard(List<Piece> data, int dataIndex){
         StringBuilder printRowBoard = new();
-        for(int x=1;x<=8;x++){
+        for(int x=1;x<=_widthBoardDimension;x++){
             int index =  data.FindIndex(p => p.pos.x == x);
             if(index==-1){
                 StringBuilder temp = new("|    ");
@@ -67,8 +76,9 @@ public static class BoardRenderer{
             }else{
                 // string pieceChoose = ColorType.White == data[index].pieceColor? playerAChara[(int)pieceWithData[index].piecesType].ToString() : playerBChara[(int)pieceWithData[index].piecesType].ToString();
                 // Console.WriteLine(data[index].piecesType);
-                string pieceChoose = GetPieceSymbol(data[index]);
-                StringBuilder temp = new($"| {pieceChoose} ");
+                string pieceType = GetPieceSymbol(data[index]);
+                string pieceColor = GetPieceColor(data[index]);
+                StringBuilder temp = new($"| {pieceType}{pieceColor} ");
                 printRowBoard.Append(temp);
             }
         }
@@ -78,9 +88,9 @@ public static class BoardRenderer{
     private static void PrintOddStringBoard(){
         StringBuilder first = new("   ");
         first.Append("+");
-        for(int i=0;i<8;i++){
-            for(int y=0;y<4;y++){
-                if(y==0 || y==4-1){
+        for(int i=0;i<_widthBoardDimension;i++){
+            for(int y=0;y<_spaces;y++){
+                if(y==0 || y==_spaces-1){
                     first.Append(" ");
                 }else{
                     first.Append("-");
